452717df46a0428223cc4dc5c51f83f58734aeab
kozaku05 <kozaku0505@gmail.com>
2025-05-18 23:52:51 +0900

    downloadまで完成

---
+++ index.js
@@ -0,0 +1,55 @@
+const rls = require("readline-sync");
+const fs = require("fs");
+const ytdl = require("@distube/ytdl-core");
+const menus = "1.ダウンロード\n2.ダウンロードタイプの選択\nend.終了\n";
+
+function menu() {
+  console.log(menus);
+  const input = rls.question("type-Number:");
+  switch (input) {
+    case "1":
+      download();
+      break;
+    case "2":
+      type();
+      break;
+    case "end":
+      return;
+    default:
+      console.log("無効な選択肢です\n");
+      menu();
+      return;
+  }
+}
+async function download() {
+  let data = fs.readFileSync("./type.json", "utf8");
+  data = JSON.parse(data);
+  switch (data.type) {
+    case "video":
+      console.log("動画をダウンロードします");
+      const url = rls.question("input-URL:");
+      const info = await ytdl.getInfo(url);
+      const title = info.player_response.videoDetails.title;
+      console.log(title);
+      console.log("ダウンロードを開始します...");
+      const stream = await ytdl(url, { quality: "highest" });
+      stream.pipe(fs.createWriteStream(title + ".mp4"));
+      stream.on("error", (error) => {
+        console.log(error);
+        console.log("ダウンロードに失敗しました");
+        return;
+      });
+      stream.on("finish", () => {
+        console.log("ダウンロードに成功しました\n");
+        menu();
+      });
+      return;
+    default:
+      console.log("type.jsonエラー");
+  }
+}
+function type() {
+  console.log("後日実装予定\n");
+  menu();
+}
+menu();
